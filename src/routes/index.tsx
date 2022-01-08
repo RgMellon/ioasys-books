0769ab5e58ab7471da22bb9181087fea3064f511
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-01-08 17:42:04 -0300

    feat: create index route to switch between authorized and not authorized routs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import { ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { useAuth } from '../hooks/auth';
 
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
-  return (
+  const { user, loading } = useAuth();
+
+  console.log(user);
+
+  return loading ? (
+    <ActivityIndicator />
+  ) : (
     <NavigationContainer>
-      <AuthRoutes />
+      {!!user && user?.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
