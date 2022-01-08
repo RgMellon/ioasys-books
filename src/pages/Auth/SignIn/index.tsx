6a9d4efeef6bd0a11c7330c7fb88974e930ddbb4
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-01-08 17:38:30 -0300

    create signIn Page

--- src/pages/Auth/SignIn/index.tsx
+++ src/pages/Auth/SignIn/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
+
+import { useAuth } from '../../../hooks/auth';
 import backgroundImage from '../../../assets/img/background-login.png';
 
 import * as S from './styles';
 
 export function SigIn() {
-  return <S.Container source={backgroundImage}></S.Container>;
+  const { signIn } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      //  const schema = Yup.object().shape({
+      //    email: Yup.string()
+      //      .required('E-mail obrigatório')
+      //      .email('Digite um e-mail válido'),
+
+      //    password: Yup.string().required('Senha obrigatória'),
+      //  });
+
+      //  await schema.validate({ email, password });
+
+      signIn({ email, password });
+    } catch (err) {
+      //  if (err instanceof Yup.ValidationError) {
+      //    Alert.alert('Opa', err.message);
+      //  } else {
+      //    Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login');
+      //  }
+    }
+  }
+
+  return (
+    <S.Container source={backgroundImage}>
+      <Input
+        label="E-mail"
+        keyboardType="email-address"
+        autoCorrect={false}
+        autoCapitalize="none"
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <S.Wrapper>
+        <Input
+          label="Password"
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        >
+          <Button title="Entrar" onPress={handleSignIn} />
+        </Input>
+      </S.Wrapper>
+    </S.Container>
+  );
 }
--- src/pages/Auth/SignIn/styles.ts
+++ src/pages/Auth/SignIn/styles.ts
@@ -3,4 +3,10 @@ import styled from 'styled-components/native';
 export const Container = styled.ImageBackground`
   flex: 1;
   padding: 20px;
+
+  justify-content: center;
+`;
+
+export const Wrapper = styled.View`
+  margin-top: 16px;
 `;
