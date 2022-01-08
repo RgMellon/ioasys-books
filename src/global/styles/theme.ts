7c041ea43de8e7f87aad9fddfd552f506c678352
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-01-08 17:37:35 -0300

    refactor: change global style

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,32 +1,18 @@
 export default {
   colors: {
-    background: '#fff',
-    text: '#050617',
-    light_text: 'rgba(23, 23, 27, 0.6)',
-    detail: '#747476',
+    background: '#E5E5E5',
+    secondary: '#ffff',
+    text: '#333333',
+    light_text: '#fff',
+    detail: '#B22E6F',
 
-    backgroundCard: {
-      grass: '#8BBE8A',
-      fire: '#FFA756',
-      water: '#58ABF6',
-      poison: '#9F6E97',
-      normal: '#B5B9C4',
-      bug: '#8BD674',
-      flying: '#748FC9',
-      eletric: '#F2CB55',
-      ground: '#F78551',
-    },
+    opacity_light_tex: 'rgba(255, 255, 255, 0.5)',
+    opacity_background: 'rgba(0, 0, 0, 0.32)',
+  },
 
-    boxType: {
-      grass: '#62B957',
-      fire: '#FD7D24',
-      water: '#4A90DA',
-      poison: '#A552CC',
-      normal: '#9DA0AA',
-      bug: '#8CB330',
-      flying: '#748FC9',
-      eletric: '#F2CB55',
-      ground: '#F78551',
-    },
+  fonts: {
+    primary_400: 'Heebo_400Regular',
+    primary_500: 'Heebo_500Medium',
+    primary_600: 'Heebo_600SemiBold',
   },
 };
