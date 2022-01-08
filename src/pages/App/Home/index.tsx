45daa690902a1abbffa6f490a5a35005e022fe52
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2022-01-08 17:41:16 -0300

    feat: create home page

--- src/pages/App/Home/index.tsx
+++ src/pages/App/Home/index.tsx
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 import { Text } from 'react-native';
+import api from '../../../services/api';
 
 export function Home() {
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  async function getBooks() {
+    try {
+      const response = await api.get('/books', {
+        params: {
+          page: '1',
+          amount: '25',
+        },
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return <Text> Home</Text>;
 }
